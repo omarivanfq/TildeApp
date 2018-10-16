473afad481bc3d59d53f2d3cfd115e65746fdd71
Alumno <Alumno@Mac-16.local>
2018-10-16 10:48:59 -0500

    Free Fall (timer, phrase and moving words with dummmy data)

---
+++ Project/FreeFallViewController.swift
@@ -0,0 +1,155 @@
+//
+//  FreeFallViewController.swift
+//  Project
+//
+//  Created by Alumno on 16/10/18.
+//  Copyright © 2018 itesm. All rights reserved.
+//
+
+import UIKit
+
+class FreeFallViewController: UIViewController {
+    
+    @IBOutlet weak var lbOption1: UIButton!
+    @IBOutlet weak var lbOption2: UIButton!
+    @IBOutlet weak var lbOption3: UIButton!
+    @IBOutlet weak var lbOption4: UIButton!
+    @IBOutlet weak var lbOption5: UIButton!
+    
+    
+    @IBAction func choose(_ sender: UIButton) {
+        chooses(opcion: sender.titleLabel!.text!)
+    }
+    
+    @IBAction func choose2(_ sender: UIButton) {
+        chooses(opcion: sender.titleLabel!.text!)
+    }
+    
+    @IBAction func choose3(_ sender: UIButton) {
+        chooses(opcion: sender.titleLabel!.text!)
+    }
+    
+    @IBAction func choose4(_ sender: UIButton) {
+        chooses(opcion: sender.titleLabel!.text!)
+    }
+    
+    @IBAction func choose5(_ sender: UIButton) {
+        chooses(opcion: sender.titleLabel!.text!)
+    }
+    
+    func chooses(opcion:String) {
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        lbResult.frame.origin.x = screenWidth * 0.5 - lbResult.frame.width * 0.5
+        lbResult.frame.origin.y = screenHeight * 0.5 - lbResult.frame.height * 0.5
+        gameOver = true
+        if (opcion == solution) {
+            lbResult.text = "¡Bien hecho!"
+        }
+        else {
+            lbResult.text = "¡Que mal!"
+        }
+    }
+    
+    var option:Option?
+    var options = [Option]()
+    
+    var timeCount:Int?
+    var timer = Timer()
+    var actTimer = Timer()
+    var labels = [UIButton]()
+    var solution:String?
+    var currentPhrase:String?
+    var optionWords = [String]()
+    var gameOver:Bool?
+    
+    @IBOutlet weak var lbPhrase: UILabel!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbTimer: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timeCount = 15
+        gameOver = false
+        lbResult.frame.origin.x = -1000
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(counter), userInfo: nil, repeats: true)
+        actTimer = Timer.scheduledTimer(timeInterval: 0.007, target: self, selector: #selector(act), userInfo: nil, repeats: true)
+        getData()
+        setLabels()
+        setOptions()
+    }
+    
+    func setLabels(){
+        labels.append(lbOption1)
+        labels.append(lbOption2)
+        labels.append(lbOption3)
+        labels.append(lbOption4)
+        labels.append(lbOption5)
+    }
+    
+    func getData() {
+        currentPhrase = "En la mesa ____ tres manzanas."
+        optionWords.append("hay")
+        optionWords.append("ay")
+        optionWords.append("ahí")
+        solution = "hay"
+    }
+    
+    func setOptions() {
+        var c = 0
+        for label in labels {
+            options.append(Option(content: optionWords[c], button: label))
+            if (c == optionWords.count - 1) {
+                c = 0
+            }
+            else {
+                c = c + 1
+            }
+        }
+        lbPhrase.text = currentPhrase!
+    }
+    
+    @objc func act() {
+        if !gameOver! {
+            for option in options {
+                option.act()
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @objc func counter() {
+        if timeCount == 0 {
+            lbTimer.text = "Game Over"
+            gameOver = true
+        }
+        else {
+            timeCount = timeCount! - 1
+            lbTimer.text = secondsToString(seconds: timeCount!)
+        }
+    }
+    
+    func secondsToString(seconds:Int) -> String {
+        let minutes = seconds / 60 % 60
+        let seconds = seconds % 60
+        return String(format:"%02d:%02d", minutes, seconds)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+
+
---
+++ Project/Option.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+class Option: NSObject {
+    
+    var content:String?
+    var button:UIButton
+    var x:CGFloat
+    var y:CGFloat
+    var speed:CGFloat
+    var up:Bool
+    
+    init(content:String, button:UIButton) {
+        self.button = button
+        self.button.setTitle(content, for: .normal)
+        self.content = content
+        x = button.frame.origin.x
+        y = button.frame.origin.y
+        self.up = arc4random_uniform(2) == 0
+        speed = 1
+    }
+    
+    func setSpeed(speed:CGFloat) {
+        self.speed = speed
+    }
+    
+    func act() {
+        let screenSize = UIScreen.main.bounds
+        let screenHeight = screenSize.height
+        
+        if self.y >= screenHeight + 20 || self.y <= -20 {
+            reallocate()
+        }
+        
+        if up {
+            self.y = self.y - (screenHeight / 700 * self.speed)
+        }
+        else {
+            self.y = self.y + (screenHeight / 700 * self.speed)
+        }
+        
+        self.button.frame.origin.y = self.y
+    }
+    
+    private func reallocate() {
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        let rand = Float(arc4random()) / Float(UINT32_MAX)
+        self.x = screenWidth * CGFloat(rand)
+        
+        self.up = arc4random_uniform(2) == 0
+        if up {
+            self.y = screenHeight
+        }
+        else {
+            self.y = 0
+        }
+        
+        button.frame.origin.x = self.x
+        button.frame.origin.y = self.y
+    }
+    
+}
