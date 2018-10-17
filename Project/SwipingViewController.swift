f2f97cefd3dda3315cd594859fa8041e2d778c37
Daniel Vazquez <luis_060198@hotmail.com>
2018-10-17 15:34:31 -0500

    Add minigame 2 functionality of moving cards

---
+++ Project/SwipingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SwipingViewController.swift
+//  Project
+//
+//  Created by Ana Perez on 10/16/18.
+//  Copyright © 2018 itesm. All rights reserved.
+//
+
+import UIKit
+
+class SwipingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
